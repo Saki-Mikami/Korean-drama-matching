84a77ed0d27700a4f6766c6dac9e40b11a37ce5d
三上紗季 <mikamisaki@mikamisakinoMacBook-Air.local>
2026-07-23 11:47:32 +0900

    Fix CORS settings for backend

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000"],
+    allow_origins=["*"],  # "*" にすると、ネットのどこからでもアクセスできるようになります
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root():
